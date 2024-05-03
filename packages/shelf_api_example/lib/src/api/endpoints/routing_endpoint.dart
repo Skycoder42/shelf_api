5a0157cf399bf71048128521800d105811cd3131
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-05-03 17:53:51 +0200

    ........

--- packages/shelf_api_example/lib/src/api/endpoints/routing_endpoint.dart
+++ packages/shelf_api_example/lib/src/api/endpoints/routing_endpoint.dart
@@ -3,54 +3,62 @@ import 'package:shelf_api/shelf_api.dart';
 class RootRoutingEndpoint extends ShelfEndpoint {
   RootRoutingEndpoint(super.request);
 
+  @Head('/')
+  Response headRoot() => Response.ok(
+        null,
+        headers: {
+          'X-INFO': 'HEAD ${this.request.handlerPath}',
+        },
+      );
+
   @Get('/')
-  String getRoot() => this.request.handlerPath;
+  String getRoot() => 'GET ${this.request.handlerPath}';
 
   @Get('/path/open')
-  String getOpenPath() => '${this.request.handlerPath}path/open';
+  String getPathOpen() => 'GET ${this.request.handlerPath}path/open';
 
   @Get('/path/closed/')
-  String getClosedPath() => '${this.request.handlerPath}path/closed/';
+  String getPathClosed() => 'GET ${this.request.handlerPath}path/closed/';
 }
 
 @ApiEndpoint('/open')
 class OpenRoutingEndpoint extends ShelfEndpoint {
   OpenRoutingEndpoint(super.request);
 
-  @Get('/')
-  String getRoot() => this.request.handlerPath;
+  @Delete('/')
+  String deleteRoot() => 'DELETE ${this.request.handlerPath}';
 
-  @Get('/path/open')
-  String getOpenPath() => '${this.request.handlerPath}path/open';
+  @Options('/path/open')
+  String optionsPathOpen() => 'OPTIONS ${this.request.handlerPath}path/open';
 
-  @Get('/path/closed/')
-  String getClosedPath() => '${this.request.handlerPath}path/closed/';
+  @Patch('/path/closed/')
+  String patchPathClosed() => 'PATCH ${this.request.handlerPath}path/closed/';
 }
 
 @ApiEndpoint('/closed/')
 class ClosedRoutingEndpoint extends ShelfEndpoint {
   ClosedRoutingEndpoint(super.request);
 
-  @Get('/')
-  String getRoot() => this.request.handlerPath;
+  @Post('/')
+  String postRoot() => 'POST ${this.request.handlerPath}';
 
-  @Get('/path/open')
-  String getOpenPath() => '${this.request.handlerPath}path/open';
+  @Put('/path/open')
+  String putPathOpen() => 'PUT ${this.request.handlerPath}path/open';
 
-  @Get('/path/closed/')
-  String getClosedPath() => '${this.request.handlerPath}path/closed/';
+  @ApiMethod(HttpMethod.trace, '/path/closed/')
+  String tracePathClosed() => 'TRACE ${this.request.handlerPath}path/closed/';
 }
 
 @ApiEndpoint('/')
 class SlashRoutingEndpoint extends ShelfEndpoint {
   SlashRoutingEndpoint(super.request);
 
-  @Get('/')
-  String getRoot() => throw Exception('THIS CAN NEVER BE REACHED');
+  @Post('/')
+  String postRoot() => 'POST ${this.request.handlerPath}slash/open';
 
-  @Get('/slash/open')
-  String getOpenPath() => 'SLASH${this.request.handlerPath}slash/open';
+  @Post('/slash/open')
+  String postSlashOpen() => 'POST ${this.request.handlerPath}slash/open';
 
-  @Get('/slash/closed/')
-  String getClosedPath() => 'SLASH${this.request.handlerPath}slash/closed/';
+  @Post('/slash/closed/')
+  String postSlashClosed() => 'POST ${this.request.handlerPath}slash/closed/';
 }
