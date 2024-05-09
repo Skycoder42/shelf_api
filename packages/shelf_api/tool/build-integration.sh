0fa3de9631da8dfaf8445f4ff8ac3a1f8effa74f
Felix Barz <Skycoder42@users.noreply.github.com>
2024-05-10 00:39:44 +0200

    Update build-integration.sh

--- packages/shelf_api/tool/build-integration.sh
+++ packages/shelf_api/tool/build-integration.sh
@@ -5,7 +5,7 @@ echo "::group::Build example server"
 cd example
 
 dart pub get
-dart run build_runner build
+dart run build_runner build --delete-conflicting-outputs
 dart compile exe lib/main.dart -o "${RUNNER_TEMP:-/tmp}/shelf-api-example-server.exe"
 rm -rf build
 
